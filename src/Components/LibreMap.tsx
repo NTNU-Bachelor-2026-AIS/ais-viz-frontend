8df06fbabbe7c13f4ecdb1cb5341c985cff12473
aleksander nesvik <aleksander-nesvik@hotmail.com>
2026-02-04 12:17:38 +0100

    added components folder, added the LibreMap component that will server as the visual map for the project.

---
+++ src/Components/LibreMap.tsx
@@ -0,0 +1,23 @@
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { useEffect, useRef } from "react";
+import style from "../CSS/LibreMap.module.css";
+
+/* 
+Function component that creates a libremap instance from libremap open source project, is not finished as not going to have style inside
+code outside of useffect and useref is from mapLibre.
+*/
+export const LibreMap = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const LibreMap = new maplibregl.Map({
+      container: "map", // container id
+      style: "https://demotiles.maplibre.org/tiles-mlt/plain.json", // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 1, // starting zoom
+    });
+  }, []);
+  return <div ref={containerRef} id="map" className={style.LibreMap}></div>;
+};
