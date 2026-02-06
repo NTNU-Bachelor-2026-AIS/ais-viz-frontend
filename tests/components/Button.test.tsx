e7c38c14629bb413c0a9bcd078318ac9518a9127
MoldyDaniel <d.s.alnes93@gmail.com>
2026-02-06 13:39:30 +0100

    Downloaded libraries to start testing in React, and set up a template.

---
+++ tests/components/Button.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+//import Button from '../../src/components/Button'
+
+// Since its using react components it is a tsx file. I am also using a fake component for now just to set up a basic template to follow in the future
+describe('Button', () => {
+    it('should render a button with the name that is provided', () => {
+       //render(<Button name="Push!" />)
+
+       // Testing if it works
+       //screen.debug
+
+       // Example of testing the button when its added.
+       //const button = screen.getByRole('button');
+       //expect(button).toBeInTheDocument();
+       //expect(button).toHaveTextContent(/push/i)
+    })
+})
