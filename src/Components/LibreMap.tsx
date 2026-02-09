bb330b326e7372af761c72d41aea8a2f0cd8fe9e
aleksander nesvik <aleksander-nesvik@hotmail.com>
2026-02-09 10:30:26 +0100

    made rendered worlds not copy and added null island vertical and horizontal lines

--- src/Components/LibreMap.tsx
+++ src/Components/LibreMap.tsx
@@ -12,12 +12,90 @@ export const LibreMap = () => {
 
   useEffect(() => {
     if (!containerRef.current) return;
-    new maplibregl.Map({
+    const map = new maplibregl.Map({
       container: "map", // container id
-      style: "https://demotiles.maplibre.org/tiles-mlt/plain.json", // style URL
+      style: "https://tiles.openfreemap.org/styles/bright", // style URL
       center: [0, 0], // starting position [lng, lat]
       zoom: 1, // starting zoom
+      renderWorldCopies: false,
+    });
+
+    map.on("load", () => {
+      map.setLayoutProperty("label_country", "text-field", [
+        "format",
+        ["get", "name_en"],
+        { "font-scale": 1.2 },
+        "\n",
+        {},
+        ["get", "name"],
+        {
+          "font-scale": 0.8,
+          "text-font": ["literal", ["Comic Sans MS"]],
+        },
+      ]);
+      map.setLayerZoomRange("label_state", 1, 24);
+      map.setLayerZoomRange("label_city", 1, 24);
+      map.setLayerZoomRange("label_town", 1, 24);
+      map.setLayerZoomRange("label_village", 1, 24);
+      map.setLayerZoomRange("label_other", 1, 24);
+
+      map.addSource("vertical_line", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: [
+              [0, -10000], // south end
+              [0, 10000],
+            ],
+          },
+        },
+      });
+      map.addLayer({
+        id: "vertical_line",
+        type: "line",
+        source: "vertical_line",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#333232",
+          "line-width": 1,
+        },
+      });
+
+      map.addSource("horizontal_line", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: [
+              [1000, 0], // south end
+              [-1000, 0],
+            ],
+          },
+        },
+      });
+      map.addLayer({
+        id: "horizontal_line",
+        type: "line",
+        source: "horizontal_line",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#333232",
+          "line-width": 1,
+        },
+      });
     });
   }, []);
+
   return <div ref={containerRef} id="map" className={style.LibreMap}></div>;
 };
