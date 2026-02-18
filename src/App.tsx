694d1c72551b265b0915f5d5075da699f6d0c436
MoldyDaniel <d.s.alnes93@gmail.com>
2026-02-18 13:08:35 +0100

    Made Ship Info card component. Following design from Kystverket.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Header
-        logo={{
-          url: "/",
-        }}
-      />
+      <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet"></link>
+        <Header
+          logo={{
+            url: "/",
+          }}
+        />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@kystverket/styrbord/style.css";
 import App from "./App.tsx";
+import ShipInfo from "./components/shipinfo/ShipInfo";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <ShipInfo />
   </StrictMode>,
 );
