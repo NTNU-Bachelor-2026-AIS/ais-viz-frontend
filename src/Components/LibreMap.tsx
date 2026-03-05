494e50a089dd07e56061b2ba0c8d37fd18fac236
aleksander nesvik <aleksander-nesvik@hotmail.com>
2026-03-05 11:17:15 +0100

    LibreMap now uses supercluster.js to calculate clusters of data and also updates the layers that need clustered data when zoom and moving map happens

--- src/Components/LibreMap.tsx
+++ src/Components/LibreMap.tsx
@@ -2,10 +2,15 @@ import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useRef } from "react";
 import style from "../CSS/LibreMap.module.css";
+import type { Point } from "geojson";
 import type { FeatureCollection } from "geojson";
 import { MapboxOverlay } from "@deck.gl/mapbox/typed";
 import { iconLayer, heatMapLayer } from "./DeckLayers";
 import { useState } from "react";
+import Supercluster, { type AnyProps, type PointFeature } from "supercluster";
+import { clusteredIconLayer } from "./ClusteredLayers";
+import { DeprecatedDetails } from "@kystverket/styrbord";
+import cluster from "cluster";
 //import { Layer } from "react-map-gl/mapbox";
 
 /* 
@@ -14,7 +19,7 @@ code outside of useffect and useref is from mapLibre.
 */
 
 interface MapProps {
-  responseData: FeatureCollection;
+  responseData: FeatureCollection<Point>;
 }
 
 export const LibreMap = ({ responseData }: MapProps) => {
@@ -22,6 +27,11 @@ export const LibreMap = ({ responseData }: MapProps) => {
   const mapRef = useRef<maplibregl.Map>(null);
   const deckOverlayRef = useRef<MapboxOverlay>(null);
   const [overLayReady, setOverLayReady] = useState<boolean>(false);
+  const [bounds, setBounds] = useState<[number, number, number, number]>([
+    0, 0, 0, 0,
+  ]);
+  const [zoom, setZoom] = useState<number>(0);
+  const clusterRef = useRef<Supercluster>(null);
 
   useEffect(() => {
     if (mapRef.current) return;
@@ -39,21 +49,50 @@ export const LibreMap = ({ responseData }: MapProps) => {
     mapRef.current.on("load", () => {
       deckOverlayRef.current = new MapboxOverlay({
         interleaved: false,
-        layers: [heatMapLayer({ responseData })],
+        //layers: [clusteredIconLayer({clusterRef.current.getClusters(bounds, zoom) as any })],
       });
       if (mapRef.current) {
         (mapRef.current.addControl as any)(deckOverlayRef.current);
         setOverLayReady(true);
       }
     });
+  }, [overLayReady, responseData]);
+
+  //useeffect for setting up the supercluster.
+  useEffect(() => {
+    clusterRef.current = new Supercluster({
+      log: true,
+      radius: 40,
+      extent: 256,
+      maxZoom: 19,
+    }).load(responseData.features as any);
+  }, [responseData]);
+
+  //kind of sloppy useeffecct to look for change in zoom and bounds after a move. think about refactoring later
+  useEffect(() => {
+    if (!mapRef.current) return;
+    mapRef.current.on("moveend", () => {
+      if (!mapRef.current) return;
+      setZoom(mapRef.current.getZoom());
+      const bounds = mapRef.current.getBounds();
+      const bbox: [number, number, number, number] = [
+        bounds.getWest(),
+        bounds.getSouth(),
+        bounds.getEast(),
+        bounds.getNorth(),
+      ];
+      setBounds(bbox);
+    });
   }, []);
 
+  //useeffect that logs when zoom and boundary changes, for testing, will then update clustering after or iconlayer ?
   useEffect(() => {
-    if (!deckOverlayRef.current) return;
-    deckOverlayRef.current.setProps({
-      layers: [iconLayer({ responseData })],
+    if (!mapRef.current || !clusterRef.current) return;
+    let clusters = clusterRef.current.getClusters(bounds, zoom);
+    deckOverlayRef.current?.setProps({
+      layers: [clusteredIconLayer({ clusters })],
     });
-  }, [responseData, overLayReady]);
+  }, [bounds, zoom]);
 
   return <div ref={containerRef} id="map" className={style.LibreMap}></div>;
 };
