df87764341df46314b51ecef70e907a49a0f56da
aleksander nesvik <aleksander-nesvik@hotmail.com>
2026-03-18 11:41:55 +0100

    added shipInfo.tsx with actual info from click on anomaly, chenged its css so that the card is above everything on the map as it should be

--- src/Components/ShipInfo/ShipInfo.tsx
+++ src/Components/ShipInfo/ShipInfo.tsx
@@ -1,45 +1,86 @@
-import React from 'react';
-import './ShipInfo.css';
+import React, { useContext, useState } from "react";
+import "./ShipInfo.css";
+import { shipInfoContext } from "./ShipInfoContext.tsx";
 
 // Interface for future props
-interface ShipInfoProps {
-  name?: string;
+export interface ShipInfoProps {
+  id?: string;
+  lastActivityAt?: string;
   mmsi?: string;
-  description?: string;
+  startedAt?: string;
+  anomalyType?: string;
+  location?: string;
 }
 
-const ShipInfo: React.FC<ShipInfoProps> = () => {
-  return (
-    <div className="ship-card">
-      {/* Top Image Section */}
-      <div className="card-image-wrapper">
-        <img 
-          src="..\src\assets\Ship Fill.webp" 
-          alt="Ship" 
-          className="ship-image" 
-        />
-      </div>
+/*
+ShipInfo component that will be a card to show information on ship and location when pressing its anomaly. 
+*/
+const ShipInfo = () => {
+  const shipInfo = useContext(shipInfoContext);
 
-      {/* Scrollable Content Section */}
-      <div className="card-content">
-        <h2 className="ship-title">Ship ship</h2>
-        <h3 className="ship-subtitle">Lorem ipsum</h3>
-        
-        <p className="ship-description">
-          This ship sent AIS message at coordinate (50, 204).
-        </p>
-
-        <div className="ship-details">
-          <p><strong>Info about ship:</strong> lorem ipsum</p>
-          <p><strong>Speed etc:</strong> 15 knots</p>
-          <p><strong>Status:</strong> Active</p>
-          <p>Additional database info in the future...</p>
+  if (shipInfo) {
+    console.log(
+      " type " +
+        shipInfo?.shipInfoProps?.anomalyType +
+        " id " +
+        shipInfo?.shipInfoProps?.id +
+        "  lastactivity " +
+        shipInfo?.shipInfoProps?.lastActivityAt +
+        " location " +
+        shipInfo?.shipInfoProps?.location +
+        " mmsi " +
+        shipInfo?.shipInfoProps?.mmsi +
+        " startedat " +
+        shipInfo?.shipInfoProps?.startedAt,
+    );
+  }
+  if (!shipInfo?.shipInfoProps)
+    return null; // code taken from https://stackoverflow.com/questions/65750698/check-if-props-are-undefined-when-loading-component
+  else
+    return (
+      <div className="ship-card">
+        {/* Top Image Section */}
+        <div className="card-image-wrapper">
+          <img
+            src="..\src\assets\Ship Fill.webp"
+            alt="Ship"
+            className="ship-image"
+          />
         </div>
 
-        <button className="edit-button">Edit Info (Admin only)</button>
+        {/* Scrollable Content Section */}
+        <div className="card-content">
+          <h2 className="ship-title">
+            Id: {shipInfo?.shipInfoProps?.id} Mmsi:{" "}
+            {shipInfo?.shipInfoProps?.mmsi}{" "}
+          </h2>
+          <h3 className="ship-subtitle">Lorem shipsum</h3>
+
+          <p className="ship-description">
+            This ship sent AIS message at coordinate{" "}
+            {shipInfo?.shipInfoProps?.location}.
+          </p>
+
+          <div className="ship-details">
+            <p>
+              <strong>Anomaly type :</strong>{" "}
+              {shipInfo?.shipInfoProps?.anomalyType}
+            </p>
+            <p>
+              <strong>Anomaly started at : </strong>{" "}
+              {shipInfo?.shipInfoProps?.startedAt}
+            </p>
+            <p>
+              <strong>Ships last activity at: </strong>{" "}
+              {shipInfo?.shipInfoProps?.lastActivityAt}
+            </p>
+            <p>New features later ?. </p>
+          </div>
+
+          <button className="edit-button">Edit Info (Admin only)</button>
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default ShipInfo;
