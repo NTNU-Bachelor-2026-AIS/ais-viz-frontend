f0fd45fa0bb1583ee8e65552f6783d20e7e37c5d
aleksander nesvik <aleksander-nesvik@hotmail.com>
2026-04-01 11:12:09 +0200

    Added feature to sidebar to display new component sidebarlistitemdetails when pressing an item in the list,

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
-import type { FeatureCollection } from "geojson";
+import type { Feature, FeatureCollection, GeoJsonProperties } from "geojson";
 import type { Point } from "geojson";
+import { SideBarListItemDetails } from "./SideBarListItemDetails";
 
 interface sideBarProps {
   responseData: FeatureCollection<Point>;
 }
 
 export const SideBar = ({ responseData }: sideBarProps) => {
-  // Dummy data representing database
+  const [query, setquery] = useState("");
+  const [anomaly, setAnomaly] = useState<Feature<
+    Point,
+    GeoJsonProperties
+  > | null>(null);
+
+  const handleSearch = (e: { target: { value: any } }) => {
+    const value = e.target.value;
+    setquery(value);
+  };
+
+  const handleItemClick = (e: Feature<Point, GeoJsonProperties> | null) => {
+    setAnomaly(e);
+  };
+
   return (
     <aside className="boat-sidebar">
       {/* Search Bar */}
@@ -17,16 +32,28 @@ export const SideBar = ({ responseData }: sideBarProps) => {
           type="text"
           placeholder="Search boat by MMSI"
           className="sidebar-search"
+          value={query}
+          onChange={handleSearch}
         />
       </div>
 
       {/* Scrollable Content Section */}
       <div className="list-container">
         {responseData.features.map((feature) => (
-          <div key={feature.properties?.id} className="boat-item">
+          <div
+            key={feature.properties?.id}
+            className="boat-item"
+            onClick={() => handleItemClick(feature)}
+          >
             <span className="boat-id">ID : {feature.properties?.id},</span>
             <span className="boat-mmsi"> MMSI: {feature.properties?.mmsi}</span>
             <span className="anomaly-type"> : {feature.properties?.type}</span>
+            {anomaly === feature ? (
+              <SideBarListItemDetails feature={feature} />
+            ) : null}{" "}
+            {/*  //
+            https://stackoverflow.com/questions/62517789/how-to-render-a-component-on-click-on-list-item-to-show-its-detail
+            here is code its based on. */}
           </div>
         ))}
       </div>
