923f2aab3cff244176a9e3d429326c6d9a02f782
MoldyDaniel <d.s.alnes93@gmail.com>
2026-04-09 14:10:35 +0200

    Added get filtered anomalies post

--- src/api/posts.tsx
+++ src/api/posts.tsx
@@ -11,7 +11,26 @@ export const getMMSI = async (
   mmsi: string,
 ): Promise<FeatureCollection<Point> | undefined> => {
   try {
-    const response = await api.get("/anomaly-groups/mmsi/" + mmsi);
+    const response = await api.get("/anomaly-groups?mmsi=" + mmsi);
+    console.log("GET Response");
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error + " an error occured");
+    return { type: "FeatureCollection", features: [] };
+  }
+};
+
+export const getFilteredAnomalies = async (params: {
+  start_date?: string;
+  end_date?: string;
+  mmsi?: string;
+  type?: string;
+}): Promise<FeatureCollection<Point> | undefined> => {
+  try {
+    const response = await api.get("/anomaly-groups", {
+      params,
+    });
     console.log("GET Response");
     console.log(response.data);
     return response.data;
