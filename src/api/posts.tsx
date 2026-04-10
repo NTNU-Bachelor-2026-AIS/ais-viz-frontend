bf700c4e4b326e2fbb4263a68d915e7138e12660
aleksander nesvik <aleksander-nesvik@hotmail.com>
2026-04-10 13:42:19 +0200

    added method to get type of anomalies from string via api, so one can search jumping for example to get all jumping anomalies only2 git status

--- src/api/posts.tsx
+++ src/api/posts.tsx
@@ -11,10 +11,44 @@ export const getMMSI = async (
   mmsi: string,
 ): Promise<FeatureCollection<Point> | undefined> => {
   try {
-    const response = await api.get("/anomaly-groups/mmsi/" + mmsi);
+    const response = await api.get("/anomaly-groups?mmsi=" + mmsi);
     console.log("GET Response");
     console.log(response.data);
     return response.data;
+  } catch (error) {
+    console.log(error + " an error occured");
+    return { type: "FeatureCollection", features: [] };
+  }
+};
+
+
+export const getTypeFromString = async (
+  anomalyType: string,
+): Promise<FeatureCollection<Point> | undefined> => {
+  try {
+    if (anomalyType.toLocaleLowerCase().includes("speed")) {
+      const response = await api.get("/anomaly-groups?type=speed_anomaly");
+      console.log("GET Response");
+      console.log(response.data);
+      return response.data;
+    }
+    if (
+      anomalyType.toLocaleLowerCase().includes("maneuver") ||
+      anomalyType.toLocaleLowerCase().includes("unexpected")
+    ) {
+      const response = await api.get(
+        "/anomaly-groups?type=unexpected_maneuver_anomaly",
+      );
+      console.log("GET Response");
+      console.log(response.data);
+      return response.data;
+    }
+    if (anomalyType.toLocaleLowerCase().includes("jumping")) {
+      const response = await api.get("/anomaly-groups?type=jumping_anomaly");
+      console.log("GET Response");
+      console.log(response.data);
+      return response.data;
+    }
   } catch (error) {
     console.log(error + " an error occured");
     return { type: "FeatureCollection", features: [] };
