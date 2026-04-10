8ef04f6fd982b92cccc811c642f393322242ee31
MoldyDaniel <d.s.alnes93@gmail.com>
2026-04-10 13:07:22 +0200

    Added Filtering method able to change layers and clustering.

--- src/Components/FilterList/FilterList.tsx
+++ src/Components/FilterList/FilterList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./SideBar.css";
+import "./FilterList.css";
 import type {
   Feature,
   FeatureCollection,
@@ -9,49 +9,58 @@ import type {
 import type { Point } from "geojson";
 import { getFilteredAnomalies } from "../../api/posts";
 
-interface getFilteredAnomalies {
-  responseData: FeatureCollection<Point>;
-}
 
 interface Props {
-    onFilterChange: (filters: any) => void;
+    onFilterSubmit: (filters: { mmsi: string, type: string, startDate: string, endDate:string }) => void;
 }
 
-export const TopFilter = ({ onFilterChange }: Props) => {
+export const FilterList = ({ onFilterSubmit }: Props) => {
     const [mmsi, setMmsi] = useState("");
     const [type, setType] = useState("");
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
+    
     const applyFilters = () => {
-    onFilterChange({
-        type,
-        start_date: startDate,
-        end_date: endDate,
+    onFilterSubmit({
+        type: type,
+        mmsi: mmsi,
+        startDate: startDate,
+        endDate: endDate,
         });
     };
+
   return (
         <div className="top-filter">
-        <select value={type} onChange={(e) => setType(e.target.value)}>
-        <option value="">All Types</option>
-        </select>
+            <select
+                value={type} 
+                onChange={(e) => setType(e.target.value)}
+                >
+            <option value="">All Types</option>
+            <option value="unexpected_maneuver_anomaly">Unexpected Maneuver Anomaly</option>
+            <option value="speed_anomaly">Speed Anomaly</option>
+            <option value="jumping_anomaly">Jumping Anomaly</option>
+            </select>
 
-        <input
-            type="mmsi"
-            value={mmsi}
-            onChange={(e) => setMmsi(e.target.value)}
-        />
+            <input
+                value={mmsi}
+                onChange={(e) => setMmsi(e.target.value)}
+            />
 
-        <input
-            type="date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-        />
+            <input
+                type="date"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+            />
 
-        <input
-            type="date"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-        />
+            <input
+                type="date"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+            />
+
+            <button onClick={applyFilters}>
+                <p>Submit</p>
+            </button>
         </div>
     );
 };
