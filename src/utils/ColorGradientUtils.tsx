1f26f1c0a6f02504fce5fd10d48f240a3a470a94
aleksander nesvik <aleksander-nesvik@hotmail.com>
2026-04-28 11:08:18 +0200

    Feat: added invidiaul anomaly to vistype Fix: removed unneeded console log for testing and changed name of method to better explain what it does

--- src/utils/ColorGradientUtils.tsx
+++ src/utils/ColorGradientUtils.tsx
@@ -5,7 +5,5 @@ uses chroma-js and returns a gradient from red to green based on values from 0 t
 */
 export const signalStrengthGradient = (singalStrenght: number) => {
   const color = chroma.scale(["red", "yellow", "green"]);
-  console.log("SIGNAL STRENGTIGNHTIHNTRNHITRNHR" + singalStrenght);
-  console.log("COLOR GRADIENT  ", color(singalStrenght));
   return color(singalStrenght);
 };
